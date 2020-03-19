68fadc2f62610554a0cc5e08d2b24f6eb858f322
Kostas Tzogka <kwstas_srok@hotmail.com>
2020-03-19 14:59:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.InputStream;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("kostas1");
+		System.out.println("kostas2");
+		System.out.println("kostas3");
+	}
+}
